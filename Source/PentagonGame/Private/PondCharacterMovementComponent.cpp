e2cd504333298c43d062ace918adeb93ba946ce1
Lum <itsluminas@gmail.com>
2026-04-14 19:51:29 +0200

    DefaultGame.ini and DefaultEngine.ini

---
+++ Source/PentagonGame/Private/PondCharacterMovementComponent.cpp
@@ -0,0 +1,137 @@
+﻿// Copyright 2020 Dan Kestranek.
+
+
+#include "PondCharacterMovementComponent.h"
+#include "PondCharacter.h"
+
+float UPondCharacterMovementComponent::GetMaxSpeed() const
+{
+	return BP_GetMaxSpeed();
+}
+
+float UPondCharacterMovementComponent::BP_GetMaxSpeed_Implementation() const
+{
+	checkf(true, TEXT("%s: should be implemented in script only!"), *FString(__FUNCTION__));
+	return -1;
+}
+
+void UPondCharacterMovementComponent::UpdateFromCompressedFlags(uint8 Flags)
+{
+	Super::UpdateFromCompressedFlags(Flags);
+
+	//The Flags parameter contains the compressed input flags that are stored in the saved move.
+	//UpdateFromCompressed flags simply copies the flags from the saved move into the movement component.
+	//It basically just resets the movement component to the state when the move was made so it can simulate from there.
+	RequestToStartSprinting = (Flags & FSavedMove_Character::FLAG_Custom_0) != 0;
+
+	RequestToStartADS = (Flags & FSavedMove_Character::FLAG_Custom_1) != 0;
+}
+
+FNetworkPredictionData_Client * UPondCharacterMovementComponent::GetPredictionData_Client() const
+{
+	check(PawnOwner != nullptr);
+
+	if (!ClientPredictionData)
+	{
+		UPondCharacterMovementComponent* MutableThis = const_cast<UPondCharacterMovementComponent*>(this);
+
+		MutableThis->ClientPredictionData = new FPondNetworkPredictionData_Client(*this);
+		MutableThis->ClientPredictionData->MaxSmoothNetUpdateDist = 92.f;
+		MutableThis->ClientPredictionData->NoSmoothNetUpdateDist = 140.f;
+	}
+
+	return ClientPredictionData;
+}
+
+void UPondCharacterMovementComponent::StartSprinting()
+{
+	RequestToStartSprinting = true;
+}
+
+void UPondCharacterMovementComponent::StopSprinting()
+{
+	RequestToStartSprinting = false;
+}
+
+void UPondCharacterMovementComponent::StartAimDownSights()
+{
+	RequestToStartADS = true;
+}
+
+void UPondCharacterMovementComponent::StopAimDownSights()
+{
+	RequestToStartADS = false;
+}
+
+void UPondCharacterMovementComponent::FPondSavedMove::Clear()
+{
+	Super::Clear();
+
+	SavedRequestToStartSprinting = false;
+	SavedRequestToStartADS = false;
+}
+
+uint8 UPondCharacterMovementComponent::FPondSavedMove::GetCompressedFlags() const
+{
+	uint8 Result = Super::GetCompressedFlags();
+
+	if (SavedRequestToStartSprinting)
+	{
+		Result |= FLAG_Custom_0;
+	}
+
+	if (SavedRequestToStartADS)
+	{
+		Result |= FLAG_Custom_1;
+	}
+
+	return Result;
+}
+
+bool UPondCharacterMovementComponent::FPondSavedMove::CanCombineWith(const FSavedMovePtr & NewMove, ACharacter * Character, float MaxDelta) const
+{
+	//Set which moves can be combined together. This will depend on the bit flags that are used.
+	if (SavedRequestToStartSprinting != ((FPondSavedMove*)&NewMove)->SavedRequestToStartSprinting)
+	{
+		return false;
+	}
+
+	if (SavedRequestToStartADS != ((FPondSavedMove*)&NewMove)->SavedRequestToStartADS)
+	{
+		return false;
+	}
+
+	return Super::CanCombineWith(NewMove, Character, MaxDelta);
+}
+
+void UPondCharacterMovementComponent::FPondSavedMove::SetMoveFor(ACharacter * Character, float InDeltaTime, FVector const & NewAccel, FNetworkPredictionData_Client_Character & ClientData)
+{
+	Super::SetMoveFor(Character, InDeltaTime, NewAccel, ClientData);
+
+	UPondCharacterMovementComponent* CharacterMovement = Cast<UPondCharacterMovementComponent>(Character->GetCharacterMovement());
+	if (CharacterMovement)
+	{
+		SavedRequestToStartSprinting = CharacterMovement->RequestToStartSprinting;
+		SavedRequestToStartADS = CharacterMovement->RequestToStartADS;
+	}
+}
+
+void UPondCharacterMovementComponent::FPondSavedMove::PrepMoveFor(ACharacter * Character)
+{
+	Super::PrepMoveFor(Character);
+
+	UPondCharacterMovementComponent* CharacterMovement = Cast<UPondCharacterMovementComponent>(Character->GetCharacterMovement());
+	if (CharacterMovement)
+	{
+	}
+}
+
+UPondCharacterMovementComponent::FPondNetworkPredictionData_Client::FPondNetworkPredictionData_Client(const UCharacterMovementComponent & ClientMovement)
+	: Super(ClientMovement)
+{
+}
+
+FSavedMovePtr UPondCharacterMovementComponent::FPondNetworkPredictionData_Client::AllocateNewMove()
+{
+	return FSavedMovePtr(new FPondSavedMove());
+}
---
+++ Source/PentagonGame/Public/PondCharacterMovementComponent.h
@@ -0,0 +1,103 @@
+﻿// Copyright 2020 Dan Kestranek.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "PondCharacterMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PENTAGONGAME_API UPondCharacterMovementComponent : public UCharacterMovementComponent
+{
+	GENERATED_BODY()
+	
+	class FPondSavedMove : public FSavedMove_Character
+	{
+	public:
+
+		typedef FSavedMove_Character Super;
+
+		///@brief Resets all saved variables.
+		virtual void Clear() override;
+
+		///@brief Store input commands in the compressed flags.
+		virtual uint8 GetCompressedFlags() const override;
+
+		///@brief This is used to check whether or not two moves can be combined into one.
+		///Basically you just check to make sure that the saved variables are the same.
+		virtual bool CanCombineWith(const FSavedMovePtr& NewMove, ACharacter* Character, float MaxDelta) const override;
+
+		///@brief Sets up the move before sending it to the server. 
+		virtual void SetMoveFor(ACharacter* Character, float InDeltaTime, FVector const& NewAccel, class FNetworkPredictionData_Client_Character & ClientData) override;
+		///@brief Sets variables on character movement component before making a predictive correction.
+		virtual void PrepMoveFor(class ACharacter* Character) override;
+
+		// Sprint
+		uint8 SavedRequestToStartSprinting : 1;
+
+		// Aim Down Sights
+		uint8 SavedRequestToStartADS : 1;
+	};
+
+	class FPondNetworkPredictionData_Client : public FNetworkPredictionData_Client_Character
+	{
+	public:
+		FPondNetworkPredictionData_Client(const UCharacterMovementComponent& ClientMovement);
+
+		typedef FNetworkPredictionData_Client_Character Super;
+
+		///@brief Allocates a new copy of our custom saved move
+		virtual FSavedMovePtr AllocateNewMove() override;
+	};
+
+public:
+	/**
+	 * Native property.
+	 */
+	UPROPERTY(ScriptReadOnly)
+	uint8 RequestToStartSprinting : 1;
+	/**
+	* Native property.
+	*/
+	UPROPERTY(ScriptReadOnly)
+	uint8 RequestToStartADS : 1;
+
+	virtual float GetMaxSpeed() const override;
+	
+	/**
+	 * @remarks Overriden in AS.
+	 * @remarks Do not override in C++.
+	 */
+	UFUNCTION(BlueprintNativeEvent)
+	float BP_GetMaxSpeed() const;
+	
+	virtual void UpdateFromCompressedFlags(uint8 Flags) override;
+	virtual class FNetworkPredictionData_Client* GetPredictionData_Client() const override;
+
+	/**
+	 * Native function.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Sprint")
+	void StartSprinting();
+	
+	/**
+	 * Native function.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Sprint")
+	void StopSprinting();
+
+	/**
+	 * Native function.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Aim Down Sights")
+	void StartAimDownSights();
+	
+	/**
+	 * Native function.
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Aim Down Sights")
+	void StopAimDownSights();
+};
